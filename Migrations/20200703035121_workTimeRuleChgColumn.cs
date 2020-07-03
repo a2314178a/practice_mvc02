f4a05b2edd89efe82d648091a2a365b40511eae7
a2314178a <a2314178a@gmail.com>
2020-07-03 17:45:19 +0800

    修正請假時遇到特殊日期可能有誤判的BUG 移除打卡報表暫存檔改成登出時做 新增彈性時間 並做當對應判斷

---
+++ Migrations/20200703035121_workTimeRuleChgColumn.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace practice_mvc02.Migrations
+{
+    public partial class workTimeRuleChgColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "lateTime",
+                table: "worktimerules");
+
+            migrationBuilder.AddColumn<int>(
+                name: "elasticityMin",
+                table: "worktimerules",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "elasticityMin",
+                table: "worktimerules");
+
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "lateTime",
+                table: "worktimerules",
+                type: "time",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0));
+        }
+    }
+}
