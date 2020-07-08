c232fd030074f0877735e43bd9d54b020024d90f
a2314178a <a2314178a@gmail.com>
2020-07-08 17:57:45 +0800

    部分JS檔拆成多個(applySign.js、message.js、setRule.js) 移除workTimeRules Unique鍵 可設定相同上下班時間(可能彈性不一樣) 但要跳警告 workTimeRules 新增 固定/排休制 請假、檢查打卡排程作相對應處理 新增請假名稱-排休

---
+++ Migrations/20200707064843_WTRuleDelUniqueAddType.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace practice_mvc02.Migrations
+{
+    public partial class WTRuleDelUniqueAddType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_worktimerules_startTime_endTime",
+                table: "worktimerules");
+
+            migrationBuilder.AddColumn<int>(
+                name: "type",
+                table: "worktimerules",
+                type: "int(1)",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "type",
+                table: "worktimerules");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_worktimerules_startTime_endTime",
+                table: "worktimerules",
+                columns: new[] { "startTime", "endTime" },
+                unique: true);
+        }
+    }
+}
