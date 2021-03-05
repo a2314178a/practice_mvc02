b4bddca5571c8d364ab15efc9aea4b99230fb080
a2314178a <a2314178a@gmail.com>
2021-03-05 16:42:36 +0800

    報表與每個月工作時數頁面新增加班時數 每個月總時長表單新增加班時長 每個月前5天也會去計算前個月的總時長

---
+++ Migrations/20210305070513_workTimeTotal_totalOvertime.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace practice_mvc02.Migrations
+{
+    public partial class workTimeTotal_totalOvertime : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "totalOvertime",
+                table: "worktimetotals",
+                nullable: false,
+                defaultValue: 0,
+                comment: "unit : minute");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "totalOvertime",
+                table: "worktimetotals");
+        }
+    }
+}
